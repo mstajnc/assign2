f865867c8767cd5cedf5f421e00e478b4483f2c9
mstajnc <u1276060@unimail.hud.ac.uk>
2016-04-06 12:19:34 +0100

    fixed storing registration data in DB

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'login', 'password',
+        'login', 'fname', 'surname', 'email', 'phone', 'password',
     ];
 
     /**
