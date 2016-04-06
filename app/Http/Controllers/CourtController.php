9576057f192ff119fd3f19151484596267384b27
mstajnc <u1276060@unimail.hud.ac.uk>
2016-04-06 22:06:49 +0100

    updated with other resource classes just in case

--- app/Http/Controllers/CourtController.php
+++ app/Http/Controllers/CourtController.php
@@ -10,21 +10,81 @@ use App\Court;
 
 class CourtController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(){
 		$courts = Court::all();
 		return view('courts.index',['panel_heading' => 'List of courts'], compact('courts'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show(Court $court){
 
 
     	return view('courts.show',['panel_heading' => 'Court details'], compact('court'));
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
      public function update(Request $request, Court $court){
     	
     	$court->update($request->all());
     	return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
