1cf62b5be4d0de7c6c1bbe3c9894c255f8b147fb
mstajnc <u1276060@unimail.hud.ac.uk>
2016-04-06 13:55:24 +0100

    edited welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Welcome to my Tennis court application's landing page.
                 </div>
             </div>
         </div>
