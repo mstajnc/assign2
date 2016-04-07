8dfc3acc4e3cbbf057f4afcbce86dfe6938b8569
mstajnc <u1276060@unimail.hud.ac.uk>
2016-04-07 12:16:13 +0100

    destroy court functionality added

--- resources/views/courts/index.blade.php
+++ resources/views/courts/index.blade.php
@@ -3,10 +3,48 @@
 @section('panel_heading', 'List of all courts')
 
 @section('content')
-	<h3>All courts</h3>
-	<ul class="list-group">
-	@foreach($courts as $court)
-		<li class="list-group-item"><a href="/courts/{{ $court->court_id}}">{{$court->court_name}} ({{$court->surface}})</a></li>
-	@endforeach
-	</ul>
+	<table class="table table-striped task-table">
+        @if (count($courts) > 0)
+        <!-- Table Headings -->
+        <thead>
+            <th>All courts</th>
+            <th>&nbsp;</th>
+        </thead>
+
+        <!-- Table Body -->
+        <tbody>
+        
+            @foreach($courts as $court)
+                <tr>
+                    <!-- Task Name -->
+                    <td class="table-text">
+                        <div><a href="/courts/{{ $court->court_id}}">{{$court->court_name}} ({{$court->surface}})</a></div>
+                    </td>                            
+                        <!-- Delete Button -->
+                        <td>
+                            <form action="/courts/{{ $court->court_id }}" method="POST">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <div class="form-group">
+	                            <div class="col-md-6 col-md-offset-4">
+	                                <button type="submit" class="btn btn-primary">
+	                                    <i class="fa fa-btn fa-exclamation-triangle"></i>Delete court
+	                                </button>
+		                            </div>
+		                        </div>
+                            </form>
+                        </td>                                                           
+                </tr>
+            @endforeach
+        @else <h2>"There are currently no courts to display."</h2>
+        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+	        <button type="submit" class="btn btn-primary">
+	            <i class="fa fa-btn fa-user"></i><a href="{{ url('/courts/create') }}">Create a court</a>
+	        </button>
+	        </div>
+            </div>
+        @endif
+        </tbody>
+    </table>
 @stop
