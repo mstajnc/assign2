a55fc30342f654bbe7f227e5c8351b55834d5e64
mstajnc <u1276060@unimail.hud.ac.uk>
2016-04-11 15:13:23 +0100

    added logic for checking permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Permission;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,22 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies($gate);
 
-        //
+        // Register permissions with Gate
+        /*foreach ($this->getPermissions() as $permission) {
+            $gate->define($permission->name, function ($user) use ($permission) {
+                return $user->hasPermission($permission);
+            });
+        }*/
+        foreach ($this->getPermissions() as $permission) {
+            $gate->define($permission->name, function ($user) use ($permission){
+                return $user->hasRole($permission->roles);
+            });
+        }
+    }
+
+    // Get all permissions
+    protected function getPermissions()
+    {
+        return Permission::with('roles')->get();
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,4 +4,8 @@
 
 @section('content')
     Welcome to my Tennis court application's landing page.
+
+    @can('create_court')
+    	<h1>testing can</h1>
+    @endcan
 @endsection
