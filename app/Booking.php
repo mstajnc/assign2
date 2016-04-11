bddfb7a3e9959370dca5edff2f867d923a529655
mstajnc <u1276060@unimail.hud.ac.uk>
2016-04-11 19:03:08 +0100

    setup user-booking relationship

--- app/Booking.php
+++ app/Booking.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Booking extends Model
 {
-    //
+
+	 protected $fillable = [
+        'booking', 'notes',
+    ];
+     public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
