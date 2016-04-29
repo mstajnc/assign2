e1fc0bf533791e94328f9e9b804c4e5aa75c7ba8
mstajnc <u1276060@unimail.hud.ac.uk>
2016-04-29 09:20:25 +0100

    added piechart to the view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,4 +5,5 @@
 @section('content')
    <p> You are logged in! </p>
    <p>In total we have <b>{{ $courts }}</b> courts, of which <b>{{ $available_courts }}</b> are available. <b>{{ $construction }}</b> are currently under construction.</p>
+   @piechart('Courts', 'courts-div', true)
 @endsection
